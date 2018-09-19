9122ff0c478eb33939874fd8eff187fad39e1077
Liton Arefin <litonice09@gmail.com>
2018-09-19 14:39:02 +0600

    Plugin Skeleton Ready

---
+++ inc/class-admin-notices.php
@@ -0,0 +1,23 @@
+<?php 
+
+/**
+ * Admin Notices
+ *
+ * @link       https://jeweltheme.com
+ * @since      1.0.0
+ *
+ * @package    Ultimate_Gutenberg
+ * @subpackage Ultimate_Gutenberg/admin
+ */
+
+/**
+ * The admin notice functionality of the plugin.
+ *
+ * @package		Ultimate_Gutenberg
+ * @subpackage  Ultimate_Gutenberg/admin
+ * @author		Jewel Theme <support@jeweltheme.com>
+ */
+
+Class Ultimate_Gutenberg_Admin_Notices{
+	
+}
--- ultimate-gutenberg-blocks.php
+++ ultimate-gutenberg-blocks.php
@@ -11,7 +11,7 @@ namespace Ultimate_Gutenberg\Gutenberg_Blocks;
  * Version:     1.0.0
  * Author:      Liton Arefin
  * Author URI:  https://jeweltheme.com
- * Text Domain: utmgutenblocks
+ * Text Domain: ugb
  * Domain Path: /languages
  * License:     GPL-3.0
  * License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
@@ -22,25 +22,146 @@ namespace Ultimate_Gutenberg\Gutenberg_Blocks;
 // No Direct Access Sire !!!
 defined('ABSPATH') || exit;
 
+
+/*
+ * Ultimate Gutenberg Constants
+ */
+
+$ugb = new Ultimate_Gutenberg();
+define( 'UGB', $ugb->plugin_name);
+define( 'UGB_VERSION', $ugb->version);
+define( 'UGB_PLUGIN_URL', $ugb->ugb_plugin_url());
+define( 'UGB_PLUGIN_DIR', $ugb->ugb_plugin_path() );
+define( 'UGB_PLUGIN_DIR_URL', $ugb->ugb_plugin_dir_url());
+define( 'UGB_IMAGE_DIR', $ugb->ugb_plugin_dir_url().'/images');
+define( 'UGB_TD', $ugb->ugb_load_textdomain());  // Ultimate Gutenberg Text Domain
+define( 'UGB_FILE', __FILE__ );
+define( 'UGB_DIR', dirname( __FILE__ ) );
+
+
+
 class Ultimate_Gutenberg{
 
-	public $version = '1.0.0';
+	public $version = "1.0.0";
+	private $plugin_path;
+	private $plugin_url;
+	private $plugin_slug;
 
 	/* Instance */
 	private static $instance;
-	
-    public static function init() {
-    static $instance = false;
 
-    if (!$instance) {
-        $instance = new Ultimate_Gutenberg();
+	/* Initialize */
+	public static function init() {
+		if ( null === self::$instance ) {
+			self::$instance = new Ultimate_Gutenberg();
+			self::$instance->ugb_include_files();
+		}
+	}
 
-        $instance->uamp_plugin_init();
+
+	/* Construction Function */
+	public function __construct(){
+
+		$this->plugin_slug    			= 'gutenberg-blocks';
+		$this->plugin_path     			= untrailingslashit( plugin_dir_path( '/', __FILE__ ) );
+		$this->plugin_url     			= untrailingslashit( plugins_url( '/', __FILE__ ) );
+
+		// Prefix using "ugb" - Ultimate Gutenberg Shortname
+		add_action( 'init', array( $this, 'ugb_register_blocks' ) );
+		add_action( 'init', array( $this, 'ugb_block_assets' ) );
+		add_action( 'init', array( $this, 'ugb_editor_assets' ) );
+		add_action( 'plugins_loaded', array( $this, 'ugb_load_dynamic_blocks' ) );
+		add_action( 'plugins_loaded', array( $this, 'ugb_load_textdomain' ) );
+		add_action( 'enqueue_block_editor_assets', array( $this, 'ugb_localization' ) );
+	}
+
+
+	/* Checks If Pro Version */
+	public function is_pro() {
+
+		if ( class_exists( 'Ultimate_Gutenberg_Pro' ) ) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+
+    /*
+     * Plugin URL
+     */
+    public function ugb_plugin_url() {
+        if ($this->plugin_url) return $this->plugin_url;
+
+        return $this->plugin_url = untrailingslashit(plugins_url('/', __FILE__));
     }
 
-    return $instance;
 
-}
+    /*
+     * Plugin Directory Path
+     */
+    public function ugb_plugin_path() {
+        if ($this->plugin_path) return $this->plugin_path;
+
+        return $this->plugin_path = untrailingslashit(plugin_dir_path(__FILE__));
+    }
+
+
+    /*
+     * Plugin Directory URL
+     */
+    public function ugb_plugin_dir_url() {
+        if ($this->plugin_dir_url) return $this->plugin_dir_url;
+
+        return $this->plugin_dir_url = untrailingslashit(plugin_dir_url(__FILE__));
+    }
+
+	/* Files Includes related with Ultimate Gutenberg Plugin */
+	public function ugb_include_files(){
+		
+		if ( is_admin() ) {
+			require_once UGB_DIR . '/inc/class-admin-notices.php';
+			require_once UGB_DIR . '/inc/get-feedback.php';
+		}
+		
+	}
+
+	/* Register Blocks */
+	public function ugb_register_blocks(){
+
+	}
+
+
+	/* Block Assets */
+	public function ugb_block_assets(){
+
+	}
+
+
+	/* Block Editor Assets */
+	public function ugb_editor_assets(){
+
+	}
+
+
+	/* Dynamic Blocks */
+	public function ugb_load_dynamic_blocks(){
+
+	}
+
+
+	/* Load Text Domain */
+	public function ugb_load_textdomain(){
+		load_plugin_textdomain('uamp', false, dirname(plugin_basename(__FILE__)) . '/languages/');
+	}
+
+
+	/* Ultimate Gutenberg Localization */
+	public function ugb_localization(){
+
+	}
+
+
 
 }
 
